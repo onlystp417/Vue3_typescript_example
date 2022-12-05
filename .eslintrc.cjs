203e968c00294bea24579f2556fe3ba4cb4e551b
Jin <onlystp417@gamil.com.tw>
2022-12-05 11:13:32 +0800

    Add ESlint and working space config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,27 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier'
-  ],
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/eslint-config-prettier', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    camelcase: 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 100,
+        trailingComma: 'none'
+      }
+    ]
   }
-}
+};
