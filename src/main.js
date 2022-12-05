66aec839f05779d9c7cd38c523dda498d89abd31
Rex <smilerex@gmail.com>
2022-12-05 10:30:28 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+import Plugin from "@quasar/quasar-ui-qcalendar/src/QCalendarMonth.js";
+import "@quasar/quasar-ui-qcalendar/src/css/calendar-month.sass";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(Plugin);
+app.mount("#app");
