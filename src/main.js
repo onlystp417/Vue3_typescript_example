23a446eb4ba9ab30cc0b7e187ee6e763de5419e9
ketyykes <xi4869@gmail.com>
2022-12-06 17:36:21 +0800

    [chore] quasar style and icon resource

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import { Quasar } from 'quasar';
+import * as QCalendarMonth from '@quasar/quasar-ui-qcalendar/src/QCalendarMonth.js';
+import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css';
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css';
+import '@quasar/extras/material-icons-round/material-icons-round.css';
 
-import Plugin from '@quasar/quasar-ui-qcalendar/src/QCalendarMonth.js';
+import 'quasar/src/css/index.sass';
 import '@quasar/quasar-ui-qcalendar/src/css/calendar-month.sass';
-
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(Plugin);
+app.use(Quasar, {
+  QCalendarMonth
+});
 app.mount('#app');
